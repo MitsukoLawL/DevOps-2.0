e65dbd6578adb714179ce04e606b7e13de417e1d
pol-dacost <islame-felipe.da-costa-fernandes@etu.unice.fr>
2016-03-21 15:50:26 +0100

    creation des modules CatalogueCostumer et CartCaishier

---
+++ CartCaisher/src/main/java/fr/unice/polytech/isa/App.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.isa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
