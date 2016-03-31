0779bb61dd4e79efd85121eea1472832ff07bfa6
Alicia MARIN <ma202240@etu.unice.fr>
2016-03-31 20:37:04 +0200

    il y a les tests sauf carTest, + classes en doublon

---
+++ cartCashier/src/test/java/arquillian/AbstractTCFTest.java
@@ -0,0 +1,41 @@
+package arquillian;
+
+import fr.unice.polytech.isa.tcf.OrderProcessing;
+import fr.unice.polytech.isa.tcf.components.KitchenBean;
+import fr.unice.polytech.isa.tcf.entities.Customer;
+import fr.unice.polytech.isa.tcf.exceptions.ExternalPartnerException;
+import fr.unice.polytech.isa.tcf.interceptors.Logger;
+import fr.unice.polytech.isa.tcf.utils.Database;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import javax.ejb.EJB;
+
+public abstract class AbstractTCFTest {
+
+
+	@EJB
+	protected Database memory;
+
+	@Deployment
+	public static WebArchive createDeployment() {
+		// Building a Web ARchive (WAR) containing the following elements:
+		return ShrinkWrap.create(WebArchive.class)
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				// Utils
+				.addPackage(Database.class.getPackage())
+				// Entities
+				.addPackage(Customer.class.getPackage())
+				// Components Interfaces
+                .addPackage(OrderProcessing.class.getPackage())
+				// Interceptors
+				.addPackage(Logger.class.getPackage())
+				// Exceptions
+				.addPackage(ExternalPartnerException.class.getPackage())
+				// Components implementations
+				.addPackage(KitchenBean.class.getPackage());
+	}
+
+}
