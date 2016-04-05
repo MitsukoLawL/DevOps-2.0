49314a3fc7778a249de742e8e3ccafc59dfe7129
islamifelipe <islamifelipe@hotmail.com>
2016-04-05 12:08:44 +0200

    WebServicesIntegrationTest

--- warGeneration/src/test/java/arquillian/AbstractTCFTest.java
+++ warGeneration/src/test/java/arquillian/AbstractTCFTest.java
@@ -7,6 +7,8 @@ import fr.unice.polytech.isa.tcf.entities.Customer;
 import fr.unice.polytech.isa.tcf.exceptions.AlreadyExistingCustomerException;
 import fr.unice.polytech.isa.tcf.interceptors.Logger;
 import fr.unice.polytech.isa.tcf.utils.Database;
+import fr.unice.polytech.isa.tcf.webservice.CartWebService;
+import fr.unice.polytech.isa.tcf.webservice.CustomerCareService;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
